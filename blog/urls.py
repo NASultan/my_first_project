532f02987da20a81b19a57b0066d9c96e24906e5
NASultan <nursultan_ak@mail.ru>
2019-02-01 21:21:45 +0600

    First version of site

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-	path('', views.post_list, name='post_list'),
+	path('', views.main, name='main'),
+	path('main', views.main, name='main'),
+	path('news', views.news, name='news'),
+	path('clean', views.clean, name='clean'),
+	path('services', views.services, name='services'),
+	path('outsourcing', views.outsourcing, name='outsourcing'),
+	path('repairs_laptop', views.repairs_laptop, name='repairs_laptop'),
+	path('repairs', views.repairs, name='repairs'),
+	path('recovery', views.recovery, name='recovery'),
+	path('assembly', views.assembly, name='assembly'),
+	path('price', views.price, name='price'),
+	path('contacts', views.contacts, name='contacts'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,2 +1,24 @@
-def post_list(request):
- 			return render(request, 'blog/post_list.html', {})
+from django.shortcuts import render
+
+def main(request):
+ 			return render(request, 'blog/ТОО НТК Sfera-S.html', {})
+def news(request):
+ 			return render(request, 'blog/новости.html', {})
+def clean(request):
+ 			return render(request, 'blog/чистка.html', {})
+def services(request):
+ 			return render(request, 'blog/аутсорсинг.html', {})
+def outsourcing(request):
+ 			return render(request, 'blog/восстановление.html', {})
+def repairs_laptop(request):
+ 			return render(request, 'blog/контакты.html', {})
+def repairs(request):
+ 			return render(request, 'blog/наши услуги.html', {})
+def recovery(request):
+ 			return render(request, 'blog/ремонт.html', {})
+def assembly(request):
+ 			return render(request, 'blog/ремонт-комп.html', {})
+def price(request):
+ 			return render(request, 'blog/сборка.html', {})
+def contacts(request):
+ 			return render(request, 'blog/сборка.html', {})
